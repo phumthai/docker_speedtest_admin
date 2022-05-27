35c2184fe201e137bd886d7c2393e3f9d4ea1237
phumthai <phumthai.spy@hotmail.com>
2022-05-27 17:05:56 +0700

    test popup

--- interspeedtest/index.php
+++ interspeedtest/index.php
@@ -7,6 +7,10 @@
   </head>
   <body>
     <div class="center">
+      <button type="submit" class="btn">Login</button>
+      
+    </div>
+    <div class="popup">
       <a href="callback.php"><button>Login with CMU IT Account</button></a>
     </div>
   </body>
