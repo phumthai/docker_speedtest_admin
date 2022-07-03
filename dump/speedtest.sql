6e1d44b426f5542054476e418007fcc0d6dc24cc
phumthai <phumthai.spy@hotmail.com>
2022-07-04 06:37:31 +0700

    fix dump

--- dump/speedtest.sql
+++ dump/speedtest.sql
@@ -15,7 +15,11 @@ CREATE TABLE IF NOT EXISTS `speedtest_users` (
     `subnet` text NULL,
     `apname` text,
     `mac` text,
-    `testcode` text
+    `testcode` text,
+    `utilize24` text,
+    `utilize5` text,
+    `clientnum24` text,
+    `clientnum5` text
 )
 ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
